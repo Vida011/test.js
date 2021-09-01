fc75d875f77def23316a41ea8479157d1cb704c1
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-09-01 14:05:30 +0300

    antras

---
+++ index1.js
@@ -0,0 +1,7 @@
+//3. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje
+// iteracijoje atspausdinti indeksą ir jo reikšmę, jeigu ta reikšmė yra lyginė (porinė)
+
+const mas3 = [889, 52, 698, 752, 9333, 7];
+
+for (let i = 0; i < mas2.length; i++) {
+    console.log(mas2[i]);
